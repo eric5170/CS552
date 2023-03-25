244d7e22156c4bb912b0f0e090528c7fa3e1b384
Yeon Jae Cho <55640905+eric5170@users.noreply.github.com>
2023-03-24 20:10:01 -0500

    prototype

--- demo1/verilog/control.v
+++ demo1/verilog/control.v
@@ -5,10 +5,24 @@
    Description     : This is the module for the overall control unit of the decode stage of the processor.
 */
 `default_nettype none
-module control (/* TODO: Add appropriate inputs/outputs for your decode stage here*/);
+module control (instr,isNotHalt,isJump,isIType1,isSignExtend, isJR, isJAL, isBranch, memToReg,memRead, memWrite, ALU_op, branch_op, ALU_src, RegWrite, RegDist, isTaken);
 
-   // TODO: Your code here
-   
+//instruction received
+input wire [15:0] instr;
+
+//control signals that result from the instruction
+output wire isNotHalt, isJump,isIType1,isSignExtend, isJR, isJAL, isBranch, memToReg,memRead, memWrite, branch_op, ALU_src, RegWrite, RegDist, isTaken;
+output wire [3:0] ALU_op;
+
+//opcode
+wire [4:0] opcode;
+wire [3:0] aluOp;
+
+
+//isIType1
+//01000, 01001, 01010, 01011, 10100, 10101, 10110, 10111, 10000, 10001, 10011
+
+
    
 endmodule
 `default_nettype wire
