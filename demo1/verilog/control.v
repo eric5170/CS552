82e96b43d4f2f6e83d3545ad6a4c0f7ce54473f3
Yeon Jae Cho <55640905+eric5170@users.noreply.github.com>
2023-03-24 19:34:25 -0500

    Created the control.v for the control unit

---
+++ demo1/verilog/control.v
@@ -0,0 +1,15 @@
+/*
+   CS/ECE 552 Spring '22
+  
+   Filename        : control.v
+   Description     : This is the module for the overall control unit of the decode stage of the processor.
+*/
+`default_nettype none
+module control (/* TODO: Add appropriate inputs/outputs for your decode stage here*/);
+
+   // TODO: Your code here
+   
+   
+endmodule
+`default_nettype wire
+
