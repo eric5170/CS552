aa009f6cf920455db4e92f178722b20b1cb2b6f1
Yeon Jae Cho <55640905+eric5170@users.noreply.github.com>
2023-03-24 19:06:18 -0500

    testing git function
    trial 1

--- demo1/verilog/fetch.v
+++ demo1/verilog/fetch.v
@@ -1,15 +1,39 @@
 /*
-   CS/ECE 552 Spring '22
+   CS/ECE 552 Spring '20
   
    Filename        : fetch.v
    Description     : This is the module for the overall fetch stage of the processor.
 */
-`default_nettype none
-module fetch (/* TODO: Add appropriate inputs/outputs for your fetch stage here*/
-		pcIn, pcOut);
+module fetch 	(
+						clk,
+						rst,
+						currPC,
+						nextPC,
+						instruction
+					);
 
-   // TODO: Your code here
+input clk, rst;
+input [15:0] currPC;
+output [15:0] nextPC, instruction;
+
+// Get instruction from INSTRUCTION MEMORY
+// Perform PC increment logic
+// Take care of HALT and NOP
+//	Dump data mem for halt
    
+// get instr from mem
+memory2c INSTR_MEM(
+				.data_out(instruction), 
+				.data_in(16'h0),  
+				.addr(currPC),  
+				.enable(1'b1),				// ???? how do we know?
+				.wr(1'b0),  
+				.createdump(1'b0),  		// ???? HALT
+				.clk(clk),  
+				.rst(rst)
+			);
+
+// Add 2 to curr address to create new addr
+cla_16b ADDER(.A(currPC), .B(16'h2), .C_in(1'b0),.S(nextPC), .C_out());
 
 endmodule
-`default_nettype wire
