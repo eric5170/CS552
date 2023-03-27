4a7299db800a019159adb20c40150f2389b3db62
jathao <91100446+jathao@users.noreply.github.com>
2023-03-27 17:42:57 -0500

    Updated Jae Register

---
+++ register.v
@@ -0,0 +1,15 @@
+module register (clk, rst, write, write_data, read_data);
+
+input clk;
+input rst;
+input write;
+input [15:0] write_data;
+output [15:0] read_data;
+
+wire [15:0] in_data;
+
+assign in_data = write ? write_data : read_data;
+
+dff dff_16[15:0] (.q(read_data), .d(in_data), .clk(clk), .rst(rst));
+
+endmodule
