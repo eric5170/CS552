5b6f57f2a4bc4752bcaff20f3b578101170bb969
jathao <91100446+jathao@users.noreply.github.com>
2023-03-27 18:20:38 -0500

    Updated Execute instead of Memory, FIXED!

--- execute.v
+++ execute.v
@@ -1,48 +1,101 @@
 /*
    CS/ECE 552 Spring '23
   
-   Filename        : memory.v
-   Description     : This module contains all components in the Memory stage of the 
-                     processor.
+   Filename        : execute.v
+   Description     : This is the overall module for the execute stage of the processor.
 */
 `default_nettype none
-module memory (
-					//	INPUTS:
-					clk,
-					rst,
+module execute (	//	INPUTS:
 					
-					//	Control Unit
-					HALT,
-					MemWrite,
+					//	Control Unit output to Execute (Exec) inputs
+					Jump,
+					isType1,
+					SignExtend,
+					JR,
+					Branch,
+					MemToReg,
 					MemRead,
+					MemWrite,
+					ALU_Op,			//	4 bits
+					Branch_Op,		//	2 bits
+					ALUSrc,
 					
-					//	ALU
-					ALU_Result,
-					read_data_2,
+					//	Register File Output to Exec inputs
+					read_data_1,	//	16 bits
+					read_data_2,	//	16 bits
 					
-					//	WriteBack
-					writeback_data,
+					//	I Type 1 & 2, and J Type inputs
+					instr,
+					
+					//	PC + 2 input
+					incPC,
 					
 					//	OUTPUTS:
-					read_data,
-					writeback_A);
+					
+					//	Execute Outputs to Data Memory
+					write_data,
+					
+					//	ALU output
+					ALU_result,
+					
+					//	Exec Output to Fetch Stage
+					next_PC);
+					
+	input wire	Jump, isType1, SignExtend, JR, Branch,
+	MemToReg, MemRead, MemWrite, ALUSrc;
 	
-	input wire	MemWrite, MemRead;
-	input wire[15:0]	ALUResult, read_data_2, writeback_data;
+	input wire[3:0]	ALU_Op;
+	input wire[1:0]	Branch_Op;
 	
-	output wire[15:0]	read_data, write_data, writeback_A;
+	input wire[15:0]	read_data_1, read_data_2, instr, incPC;
 	
-	wire	MemReadOrWrite;
+	output wire[15:0]	write_data,	ALU_result, next_PC;
 	
-	assign	MemReadOrWrite = MemRead | MemWrite;
+	wire	ALU_B, IT1_sign_extend, IT1_zero_extend, IT1_extend,
+			IT2_extend, IType, J_extend, 
+			offset, inc_Offset, notJR_PC, zero, isTaken, toBranch;
+			
+	wire	IT1, IT2, J;
 	
-	memory2c DATA_MEMORY(.data_out(read_data), .data_in(read_data_2), 
-		.addr(ALU_Result), .enable(MemReadOrWrite), .wr(MemWrite), 
-		.createdump(HALT), .clk(clk), .rst(rst));
-
-	assign writeback_A = ALU_Result;
-	assign write_data = writeback_data;
-
+	assign	IT1	=	instr[4:0];
+	assign	IT2 =	instr[7:0];
+	assign	J	=	instr[10:0];
+	
+	assign	IT1_sign_extend = { {11{IT1[4]}}, IT1 };
+	assign	IT1_zero_extend = { {11{1'b0}}, IT1 };
+	mux2_1	ITYPE_EXTEND(.out(IT1_extend), .inputA(IT1_zero_extend),
+		.inputB(IT1_sign_extend), .sel(SignExtend));
+	
+	assign	IT2_extend = { {8{IT2[7]}}, IT2 };
+	mux2_1	ITYPE_MUX(.out(IType), .inputA(IT2_extend), 
+		.inputB(IT1_extend), .sel(isType1));
+	
+	assign	J_extend = 	{ {5{J[10]}}, J };
+	mux2_1 OFFSET_MUX(.out(offset), .inputA(J_extend),
+		.inputB(IType), .sel(Branch));
+	
+	cla16b OFFSET_ADDER(.sum(inc_Offset), .cOut(), .inA(offset),
+		.inB(incPC), .cIn(1'b0));
+	
+	assign isTaken = zero & Branch;
+	
+	assign toBranch = isTaken | Jump;
+	
+	mux2_1 NOTJR_MUX(.out(notJR_PC), .inputA(incPC), 
+		.inputB(inc_Offset), .sel(toBranch));
+	
+	mux2_1 ALU_B_MUX (.out(ALU_B), .inputA(IType),
+		.inputB(read_data_2), .sel(ALUSrc));
+	
+	aluhier(.InA(read_data_1), .InB(ALU_B), .Cin(),
+		.Oper(ALU_Op), .sign(SignExtend), .Out(ALU_Result),
+		.Zero(zero), .Of1());
+	
+	mux2_1 NEXT_PC_MUX(.out(next_PC), .inputA(notJR_PC),
+		.inputB(ALU_Result), .sel(JR));
+	
+	assign	write_data = read_data_2;
+	
    
 endmodule
 `default_nettype wire
