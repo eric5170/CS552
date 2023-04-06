55fc0e78b593db240aa2bfdb2b4cd99a827d3fbb
Yeon Jae Cho <55640905+eric5170@users.noreply.github.com>
2023-04-06 16:43:28 -0500

    added hazard detection unit

---
+++ demo2/verilog/hazard_detect.v
@@ -0,0 +1,49 @@
+module hazard_detect(		instr,
+                            writeRegSel_DX, 
+                            writeRegSel_XM, 
+                            readRegSel1, 
+                            readRegSel2, 
+                            isRegWrite_DX,
+                            isRegWrite_XM,
+                            stall
+                            );
+
+    input wire [15:0] instr;
+    input wire [2:0] writeRegSel_DX, writeRegSel_XM, readRegSel1, readRegSel2;
+    input wire isRegWrite_DX, isRegWrite_XM;
+    output stall;
+	
+	wire [1:0] num;
+    reg r1, r2;
+    
+    regRead iregRead(.instr(instr),
+			.num(num));
+
+    always@(*) begin
+        case(num)
+                2'b00: begin
+                        r1 = 0;
+                        r2 = 0;
+                    end
+                2'b01: begin
+                        r1 = 1;
+                        r2 = 0;
+                    end
+                2'b10: begin	
+                        r1 = 1;
+                        r2 = 1;			
+                    end
+                default: begin
+                        r1 = 0;
+                        r2 = 0;
+                    end
+        endcase
+    end
+
+    assign stall = (( r1 & (readRegSel1 == writeRegSel_DX) & (isRegWrite_DX)) 
+                    | (r1 & (readRegSel1 == writeRegSel_XM) & (isRegWrite_XM))
+                   | (r2 & (readRegSel2 == writeRegSel_DX) & (isRegWrite_DX))
+                   | (r2 & (readRegSel2 == writeRegSel_XM) & (isRegWrite_XM))
+                   ) 
+                   ? 1 : 0;
+endmodule
---
+++ demo2/verilog/regRead.v
@@ -0,0 +1,108 @@
+module regRead(instr, num);
+	input wire [15:0] instr;
+	output wire [1:0] num;
+
+	wire [4:0] opcode;
+	assign opcode = instr[15:11];
+	reg [1:0] num_temp;
+	assign num_reg_reads = num_temp;
+
+	always@(*) begin
+	   case(opcode)
+			5'b00000: begin			/************************************ HALT */
+						num_temp = 2'h0;
+					 end
+			5'b00001: begin			/************************************ NOP */
+						num_temp = 2'h0;
+					 end
+			5'b00100: begin			/************************************ J */
+						num_temp = 2'h0;
+					 end
+			5'b00110: begin			/************************************ JAL */
+						num_temp = 2'h0;
+					 end
+			5'b01000: begin			/************************************ ADDI */
+						num_temp = 2'h1;
+					 end
+			5'b01001: begin			/************************************ SUBI */
+						num_temp = 2'h1;
+					 end
+			5'b01010: begin			/************************************ XORI */
+						num_temp = 2'h1;
+					 end
+			5'b01011: begin			/************************************ ANDNI */
+						num_temp = 2'h1;
+					 end
+			5'b10100: begin			/************************************ ROLI */
+						num_temp = 2'h1;
+					 end
+			5'b10101: begin			/************************************ SLLI */
+						num_temp = 2'h1;
+					 end
+			5'b10110: begin			/************************************ RORI */
+						num_temp = 2'h1;
+					 end
+			5'b10111: begin			/************************************ SRLI */
+						num_temp = 2'h1;
+					 end
+			5'b10000: begin			/************************************ ST */
+						num_temp = 2'h2;
+					 end
+			5'b10001: begin			/************************************ LD */
+						num_temp = 2'h1;
+					 end
+			5'b10011: begin			/************************************ STU */
+						num_temp = 2'h2;
+					 end
+			5'b11000: begin			/************************************ LBI */
+						num_temp = 2'h0;
+					 end
+			5'b10010: begin			/************************************ SLBI */
+						num_temp = 2'h1;
+					 end
+			5'b00101: begin			/************************************ JR */
+						num_temp = 2'h1;
+					 end
+			5'b00111: begin			/************************************ JALR */
+						num_temp = 2'h1;
+					 end
+			5'b01100: begin			/************************************ BEQZ */
+						num_temp = 2'h1;
+					 end
+			5'b01101: begin			/************************************ BNEZ */
+						num_temp = 2'h1;
+					 end
+			5'b01110: begin			/************************************ BLTZ */
+						num_temp = 2'h1;
+					 end
+			5'b01111: begin			/************************************ BGEZ */
+						num_temp = 2'h1;
+					 end
+			5'b11001: begin			/************************************ BTR */
+						num_temp = 2'h1;
+					 end
+			5'b11011: begin			/************************************ ADD, SUB, XOR, ANDN */
+						num_temp = 2'h2;
+					 end
+			5'b11010: begin			/************************************ ROL, SLL, ROR, SRL */
+						num_temp = 2'h2;
+					 end
+			5'b11100: begin			/************************************ SEQ */
+						num_temp = 2'h2;
+					 end
+			5'b11101: begin			/************************************ SLT */
+						num_temp = 2'h2;
+					 end
+			5'b11110: begin			/************************************ SLE */
+						num_temp = 2'h2;
+					 end
+			5'b11111: begin			/************************************ SCO */
+						num_temp = 2'h2;
+					 end
+			default: begin
+						num_temp = 2'h0;
+					end
+		endcase
+	end
+
+endmodule
