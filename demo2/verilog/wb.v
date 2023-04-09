e843148541eab0f17f603dfa4343892a848a890a
jathao <91100446+jathao@users.noreply.github.com>
2023-04-08 21:51:46 -0500

    Removed Write_en

--- demo2/verilog/wb.v
+++ demo2/verilog/wb.v
@@ -4,16 +4,16 @@
    Filename        : wb.v
    Description     : This is the module for writeback logic.
 */
-module wb(readData,	isMemToReg, isMemRead, aluResult, nextPC, isJAL, writeData,	writeEn, writeRegSel, writeReg);
+module wb(readData,	isMemToReg, isMemRead, aluResult, nextPC, isJAL, writeData, writeRegSel, writeReg);
 
 	input [15:0] readData, aluResult, nextPC;
 	input [2:0] writeRegSel;
-	input isMemRead, isMemToReg, isJAL, writeEn;
+	input isMemRead, isMemToReg, isJAL;
 	
 	output [15:0] writeData;
 	output [2:0] writeReg;
 	
-	assign writeData = writeEn? (isJAL ? nextPC : (isMemToReg ? readData : aluResult)) : writeData;
+	assign writeData = isJAL ? nextPC : (isMemToReg ? readData : aluResult);
 	assign writeReg = writeRegSel;
 	
 endmodule
