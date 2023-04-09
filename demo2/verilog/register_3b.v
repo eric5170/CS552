c0e5356ab029789aeec48fac498e5f2b2d6cf78d
Yeon Jae Cho <55640905+eric5170@users.noreply.github.com>
2023-04-09 05:37:39 -0500

    Delete register_3b.v
    changed to register3b.v

--- demo2/verilog/register_3b.v
+++
@@ -1,8 +0,0 @@
-module register_3b(en, clk, rst, state, data_in);
-	input clk, rst, en;
-	input [2:0] data_in;
-	output [2:0] state;
-
-	dff_en DFF[2:0] (.en(en), .clk(clk), .rst(rst), .d(data_in), .q(state));
-
-endmodule
