e509380bcde94ea5001bbd4b329896c78b256d34
Yeon Jae Cho <55640905+eric5170@users.noreply.github.com>
2023-04-09 05:37:20 -0500

    Delete register_1b.v
    changed to register1b.v

--- demo2/verilog/register_1b.v
+++
@@ -1,8 +0,0 @@
-module register_1b(en, clk, rst, state, data_in);
-input clk, rst, en;
-input data_in;
-output state;
-
-dff_en DFF(.en(en), .clk(clk), .rst(rst), .d(data_in), .q(state));
-
-endmodule
