31ce5aba4cc6efd0ce0e2672723c50b97851709d
jathao <91100446+jathao@users.noreply.github.com>
2023-04-09 20:32:56 -0500

    Simplified DFF_en

--- demo2/verilog/register3b.v
+++ demo2/verilog/register3b.v
@@ -2,9 +2,8 @@ module register3b(en, clk, rst, state, data_in);
 	input wire clk, rst, en;
 	input wire [2:0] data_in;
 	output wire [2:0] state;
-		
-	dff_en dff0(.en(en), .clk(clk), .rst(rst), .d(data_in[0]), .q(state[0]));
-	dff_en dff1(.en(en), .clk(clk), .rst(rst), .d(data_in[1]), .q(state[1]));
-	dff_en dff2(.en(en), .clk(clk), .rst(rst), .d(data_in[2]), .q(state[2]));
+	
+	
+	dff_en DFF_EN_SERIES[2:0] (.en(en), .clk(clk), .rst(rst), .d(data_in), .q(state));
 
 endmodule
