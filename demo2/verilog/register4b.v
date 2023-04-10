2912f517314f80ab707b535e11ff14434aa367a0
jathao <91100446+jathao@users.noreply.github.com>
2023-04-09 20:33:31 -0500

    Simplified DFF_en

--- demo2/verilog/register4b.v
+++ demo2/verilog/register4b.v
@@ -4,9 +4,6 @@ module register4b(en, clk, rst, data_in, state);
     input wire[3:0] data_in;
     output wire [3:0] state;
 
-    dff_en dff0(.en(en), .clk(clk), .rst(rst), .d(data_in[0]), .q(state[0]));
-    dff_en dff1(.en(en), .clk(clk), .rst(rst), .d(data_in[1]), .q(state[1]));
-    dff_en dff2(.en(en), .clk(clk), .rst(rst), .d(data_in[2]), .q(state[2]));
-    dff_en dff3(.en(en), .clk(clk), .rst(rst), .d(data_in[3]), .q(state[3]));
+    dff_en DFF_EN_SERIES[3:0] (.en(en), .clk(clk), .rst(rst), .d(data_in), .q(state));
 
 endmodule
