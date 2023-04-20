4f4f865ad34cca104f77bf813f79ab20c81f8d96
jathao <91100446+jathao@users.noreply.github.com>
2023-04-20 18:17:36 -0500

    Upload Files

---
+++ cache/cache_assoc/verilog/register.v
@@ -0,0 +1,16 @@
+/*
+   CS/ECE 552 Spring '23
+  
+   Filename        : register.v
+   Description     : This is the module for register logic using dff provided
+*/
+
+module register(clk, rst, data_in, state);
+	input clk, rst;
+	input [15:0] data_in;
+
+	output [15:0] state;
+	
+	dff DFF_SERIES[15:0] (.q(state), .d(data_in), .clk(clk), .rst(rst));
+
+endmodule
