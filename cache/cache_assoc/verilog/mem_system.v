f8d2ca53b19cf1c3353316ba6b66648bb6b26070
Yeon Jae Cho <55640905+eric5170@users.noreply.github.com>
2023-04-23 15:23:50 -0500

    updated working code

--- cache/cache_assoc/verilog/mem_system.v
+++ cache/cache_assoc/verilog/mem_system.v
@@ -137,13 +137,22 @@ module mem_system(/*AUTOARG*/
               .cache_hit(CacheHit_i)
 					);
    	// assigning the outputs 
-	always @(*)	begin
-		Done = Done_i;
-		Stall = Stall_i;
-		CacheHit = CacheHit_i;
-		DataOut = DataOut_i;
-	end	
-	
+	always @(*) begin
+		case (1'bx)
+			Done_i: Done = Done_i;
+			Stall_i: Stall = Stall_i;
+			err_i: err = err_i;
+			CacheHit_i: CacheHit = CacheHit_i;
+			DataOut_i: DataOut = DataOut_i;
+			default: begin
+				Done = Done_i;
+				 Stall = Stall_i;
+				 err = err_i;
+				CacheHit = CacheHit_i;
+				 DataOut = DataOut_i;
+			end
+		endcase
+	end
 	
 endmodule
 
