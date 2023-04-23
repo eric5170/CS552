94c7660ec94b2a4bd10b960e717b236088476907
jathao <91100446+jathao@users.noreply.github.com>
2023-04-23 17:48:07 -0500

    Update code

--- cache/cache_assoc/verilog/ps_random_logic.v
+++ cache/cache_assoc/verilog/ps_random_logic.v
@@ -1,4 +1,5 @@
 module ps_random_logic(
+							//	Inputs
 							input wire clk,
 							input wire rst,
 							input wire hit1,
@@ -12,6 +13,8 @@ module ps_random_logic(
 							input wire [4:0] tag2,
 							input wire [15:0] data1,
 							input wire [15:0] data2,
+							
+							//	Outputs
 							output wire hit,
 							output wire dirty,
 							output wire repl_en1,
@@ -26,19 +29,15 @@ module ps_random_logic(
 	
 	dff_en iDFF(.en(flip), .q(victimway), .d(~victimway), .clk(clk), .rst(rst));
 
-	// both cache blocks valid --> output of flip flop
-	// otherwise --> way0 = valid --> way1
-	// else --> way0
-	assign target = (valid1 & valid2) ? ~victimway: valid1 ? 1 : 0;
+	assign target = (valid1 & valid2) ? ~victimway : valid1;
 
-	// Intermediate signals for replacement logic
 	assign target_tag = target ? tag2 : tag1;                   
 	assign target_data = target ? data2 : data1;           
-	assign hit_tag = (hit1 & valid1) ? tag1: tag2;                         
+	assign hit_tag = (hit1 & valid1) ? tag1 : tag2;                         
 	assign hit_data = (hit1 & valid1) ? data1 : data2;                
 
-	assign repl_en1 = target ? 0 : 1;                      
-	assign repl_en2 = target ? 1 : 0;                     
+	assign repl_en1 = ~target;                      
+	assign repl_en2 = target;                     
 	assign hit = (hit1 & valid1) | (hit2 & valid2);                    
 	assign dirty = target ? dirty2 : dirty1;                           
 	assign repl_data_out = hit ? hit_data : target_data;    
