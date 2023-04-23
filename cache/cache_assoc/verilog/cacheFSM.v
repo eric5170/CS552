ff66d7e76a7a4075f9c2753f22d3bce2431804a2
jathao <91100446+jathao@users.noreply.github.com>
2023-04-23 17:53:58 -0500

    Update Code

--- cache/cache_assoc/verilog/cacheFSM.v
+++ cache/cache_assoc/verilog/cacheFSM.v
@@ -1,106 +1,81 @@
-`define IDLE 0
-`define COMPARE_READ 1
-`define COMPARE_WRITE 2
-`define EVICT_0 3
-`define EVICT_1 4
-`define EVICT_2 5
-`define EVICT_3 6
-`define ALLOC_0 7
-`define ALLOC_1 8
-`define ALLOC_2 9
-`define ALLOC_3 10
-`define AW_0 11
-`define AW_1 12
-`define AW_2 13
-`define AW_3 14
-
 module cacheFSM(
-                ld,
-                st,
-                address,
-                hit1,
-                hit2,
-                dirty1,
-                dirty2,
-                valid1,
-                valid2,
-                data_in1,
-                data_in2,
-                tag_out1,
-                tag_out2,
-                data_in_mem,
-                mem_stall,
-                enable1,
-                enable2,
-                comp,
-                write,
-                tag_in,
-                cache_data_in,
-                valid_in,
-                offset_in,
-                done,
-                mem_addr,
-                mem_data,
-                wr,
-                rd,
-                clk,
-                rst,
-                data_out_sys,
-                data_in_sys,
-                sys_stall,
-                cache_hit
+				//	Inputs
+				input wire clk, 
+				input wire rst,
+				input wire ld, 
+				input wire st, 
+				input wire hit1, 
+				input wire hit2, 
+				input wire dirty1, 
+				input wire dirty2, 
+				input wire valid1, 
+				input wire valid2, 
+				input wire mem_stall,
+				input wire [15:0] address, 
+				input wire [15:0] data_in1, 
+				input wire [15:0] data_in2, 
+				input wire [15:0] data_in_mem, 
+				input wire [15:0] data_in_sys,
+				input wire [4:0] tag_out1, 
+				input wire [4:0] tag_out2,
+				
+				//	Outputs
+				output reg enable1, 
+				output reg enable2, 
+				output reg comp, 
+				output reg write, 
+				output reg done, 
+				output reg wr, 
+				output reg rd, 
+				output reg valid_in,
+				output reg sys_stall, 
+				output reg cache_hit,
+				output reg [15:0] mem_addr, 
+				output reg [15:0] mem_data, 
+				output reg [15:0] cache_data_in, 
+				output reg [15:0] data_out_sys,
+				output reg [4:0] tag_in,
+				output reg [2:0] offset_in
 );
 
-    input clk, rst;
-    input ld, st, hit1, hit2, dirty1, dirty2, valid1, valid2, mem_stall;
-    input [15:0] address, data_in1, data_in2, data_in_mem, data_in_sys;
-    input [4:0] tag_out1, tag_out2;
-    
-    output reg enable1, enable2, comp, write, done, wr, rd, valid_in;
-    output reg [15:0] mem_addr, mem_data, cache_data_in;
-    output reg [4:0] tag_in;
-    output reg [2:0] offset_in;
-    output reg [15:0] data_out_sys;
-    output reg sys_stall;
-    output reg cache_hit;
+	//	Local parameters for the state names
+	localparam IDLE				=	4'b0000; // 0
+	localparam COMPARE_READ 	=	4'b0001; // 1
+	localparam COMPARE_WRITE	=	4'b0010; // 2
+	localparam EVICT_0			=	4'b0011; // 3
+	localparam EVICT_1			=	4'b0100; // 4
+	localparam EVICT_2			=	4'b0101; // 5
+	localparam EVICT_3			=	4'b0110; // 6
+	localparam ALLOC_0			=	4'b0111; // 7
+	localparam ALLOC_1			=	4'b1000; // 8
+	localparam ALLOC_2			=	4'b1001; // 9
+	localparam ALLOC_3			=	4'b1010; // 10
+	localparam AW_0				=	4'b1011; // 11
+	localparam AW_1				=	4'b1100; // 12
+	localparam AW_2				=	4'b1101; // 13
+	localparam AW_3				=	4'b1110; // 14
 
-    wire [15:0] state;
-    reg [15:0] next_state;
-    reg ld_reg, st_reg;
-    reg [15:0] address_reg, data_in_sys_reg;
-    reg replace_enable_1_reg, replace_enable_2_reg, replace_hit_reg, replace_dirty_reg;
-    reg [15:0] replace_data_out_reg;
-    reg [4:0] replace_tag_in_reg;
-
-
-    wire [4:0] tag;
-    wire [7:0] index;
+    wire [15:0] state, replace_data_out;
+	wire [7:0] index;
+	wire [4:0] replace_tag_in;
     wire [2:0] offset;
-
-    wire [15:0] replace_data_out;
-    wire [4:0] replace_tag_in;
-    wire replace_hit, replace_dirty, replace_enable_1, replace_enable_2, ld_or_st;
-    reg flip_victimway;
+	wire replace_hit, replace_dirty, replace_enable_1, replace_enable_2, ld_or_st;
+	
+    reg [15:0] next_state, address_reg, data_in_sys_reg, replace_data_out_reg;
+    reg [4:0] replace_tag_in_reg, tag;
+    reg ld_reg, st_reg, replace_enable_1_reg, replace_enable_2_reg, replace_hit_reg, replace_dirty_reg, flip_victimway;
 
     assign tag = address_reg[15:11];
     assign index = address_reg[10:3];
     assign offset = address_reg[2:0];
     assign ld_or_st = ld | st;
 
-    // Check if we have a ld or store, if neither, stay in IDLE (FIXED)
-    // Output stall signal from FSM - every state but IDLE (FIXED)
-    // distinguish between mem_stall (in) and cacheFSM_stall (out) (FIXED)
-    // Done sig = data out ready
-    // We want to wait 2 CYCLES after mem reads - have to add 2 more states (prob)
-    //         Alloc0 -> Alloc1 -> Alloc2(Alloc0 available).. (one cycle per alloc)
-    // Make state a flip flop instead of always (no posedge or negedge) (FIXED)
-
-    /** CACHE FSM **/
+    // CACHE FSM
     always @(*) begin
         flip_victimway = 0;
         case(state)
-            `IDLE: begin // Check if it's neither ld or st - stay in IDLE
-                        next_state = ld ? `COMPARE_READ : (st ? `COMPARE_WRITE : `IDLE);
+            IDLE: begin
+						next_state = ld_or_st ? (ld ? COMPARE_READ : COMPARE_WRITE) : IDLE;
                         done = 1'h0;
                         sys_stall = 1'h0;
                         enable1 = 0;
@@ -115,10 +90,11 @@ module cacheFSM(
                         replace_hit_reg = 0;
                         flip_victimway = 0;
                     end
-            `COMPARE_READ: begin
-                        next_state = replace_hit ? `IDLE : ((~replace_hit & replace_dirty) ? `EVICT_0 : 
-                                            ((~replace_hit | ~replace_dirty) ? `ALLOC_0 : `COMPARE_READ));
-                        // Cache Inputs
+					
+            COMPARE_READ: begin
+						next_state = replace_hit ? IDLE : (replace_dirty ? EVICT_0 : ALLOC_0);
+						
+                        //	Cache Inputs
                         comp = 1;
                         write = 0;
                         enable1 = 1;
@@ -127,7 +103,8 @@ module cacheFSM(
                         offset_in = offset;
                         valid_in = 1;
                         cache_data_in = data_in_sys;
-                        // Assign registers for the current operation
+						
+                        //	Assign registers
                         st_reg = st;
                         ld_reg = ld;
                         data_in_sys_reg = data_in_sys;
@@ -139,15 +116,17 @@ module cacheFSM(
                         replace_data_out_reg = replace_data_out;
                         replace_tag_in_reg = replace_tag_in;
                         flip_victimway = ld_or_st;
-                        // Mem sys outputs
+						
+                        //	Mem sys outputs
                         sys_stall = 1;
-                        done = replace_hit ? 1'h1 : 1'h0;
+                        done = replace_hit;
                         data_out_sys = replace_hit ? replace_data_out : data_out_sys;
                         cache_hit = replace_hit;
                     end
-            `COMPARE_WRITE: begin
-                        next_state = (replace_hit ? `IDLE : ((~replace_hit & replace_dirty) ? `EVICT_0 : 
-                                            ((~replace_hit | ~replace_dirty)) ? `ALLOC_0 : `COMPARE_WRITE));
+					
+            COMPARE_WRITE: begin
+                        next_state = replace_hit ? IDLE : (replace_dirty ? EVICT_0 : ALLOC_0);
+						
                         // Cache Inputs
                         comp = 1;
                         write = 1;
@@ -157,7 +136,8 @@ module cacheFSM(
                         offset_in = offset;
                         valid_in = 1;
                         cache_data_in = data_in_sys;
-                        // Assign registers for the current operation
+						
+                        // Assign registers
                         st_reg = st;
                         ld_reg = ld;
                         data_in_sys_reg = data_in_sys;
@@ -169,20 +149,24 @@ module cacheFSM(
                         replace_data_out_reg = replace_data_out;
                         replace_tag_in_reg = replace_tag_in;
                         flip_victimway = ld_or_st;
+						
                         // Mem sys outputs
                         sys_stall = 1;
-                        done = replace_hit ? 1'h1 : 1'h0;
+                        done = replace_hit;
                         data_out_sys = replace_hit ? replace_data_out : data_out_sys;
                         cache_hit = replace_hit;
                     end
-            `EVICT_0: begin
-                        next_state = `EVICT_1;
+					
+            EVICT_0: begin
+                        next_state = EVICT_1;
+						
                         // Memory access settings
                         wr = 1;
                         rd = 0;
                         mem_addr = {replace_tag_in_reg, index, 3'h0};
                         mem_data = replace_enable_1_reg ? data_in1 : data_in2;
                         sys_stall = 1;
+						
                         // Caches
                         enable1 = replace_enable_1_reg;
                         enable2 = replace_enable_2_reg;
@@ -192,14 +176,17 @@ module cacheFSM(
                         offset_in = 3'h0;
                         valid_in = 1;
                     end
-            `EVICT_1: begin
-                        next_state = `EVICT_2;
+					
+            EVICT_1: begin
+                        next_state = EVICT_2;
+						
                         // Memory access settings
                         wr = 1;
                         rd = 0;
                         mem_addr = {replace_tag_in_reg, index, 3'h2};
                         mem_data = replace_enable_1_reg ? data_in1 : data_in2;
                         sys_stall = 1;
+						
                         // Caches
                         enable1 = replace_enable_1_reg;
                         enable2 = replace_enable_2_reg;
@@ -209,14 +196,17 @@ module cacheFSM(
                         offset_in = 3'h2;
                         valid_in = 1;
                     end
-            `EVICT_2: begin
-                        next_state = `EVICT_3;
+					
+            EVICT_2: begin
+                        next_state = EVICT_3;
+						
                         // Memory access settings
                         wr = 1;
                         rd = 0;
                         mem_addr = {replace_tag_in_reg, index, 3'h4};
                         mem_data = replace_enable_1_reg ? data_in1 : data_in2;
                         sys_stall = 1;
+						
                         // Caches
                         enable1 = replace_enable_1_reg;
                         enable2 = replace_enable_2_reg;
@@ -225,15 +215,18 @@ module cacheFSM(
                         tag_in = replace_tag_in_reg;
                         offset_in = 3'h4;
                         valid_in = 1;
-                    end        
-            `EVICT_3: begin
-                        next_state = `ALLOC_0;
+                    end     
+					
+            EVICT_3: begin
+                        next_state = ALLOC_0;
                         // Memory access settings
+						
                         wr = 1;
                         rd = 0;
                         mem_addr = {replace_tag_in_reg, index, 3'h6};
                         mem_data = replace_enable_1_reg ? data_in1 : data_in2;
                         sys_stall = 1;
+						
                         // Caches
                         enable1 = replace_enable_1_reg;
                         enable2 = replace_enable_2_reg;
@@ -243,110 +236,128 @@ module cacheFSM(
                         offset_in = 3'h6;
                         valid_in = 1;
                     end
-            `ALLOC_0: begin
-                        next_state = `ALLOC_1;
+					
+            ALLOC_0: begin
+                        next_state = ALLOC_1;
+						
                         // Memory access settings
                         wr = st_reg & (offset == 3'h0);
                         rd = ld_reg | (st_reg & (offset != 3'h0));
                         mem_addr = {address_reg[15:3], 3'h0}; 
                         mem_data = data_in_sys_reg;
                         sys_stall = 1;
+						
                         // Disable caches
                         enable1 = 0;
                         enable2 = 0;
                         write = 0;
                         comp = 0;
                     end
-            `ALLOC_1: begin
-                        next_state = `ALLOC_2;
+					
+            ALLOC_1: begin
+                        next_state = ALLOC_2;
+						
                         // Memory access settings
                         wr = st_reg & (offset == 3'h2);
                         rd = ld_reg | (st_reg & (offset != 3'h2));
                         mem_addr = {address_reg[15:3], 3'h2};
                         mem_data = data_in_sys_reg;
                         sys_stall = 1;
+						
                         // Disable caches
                         enable1 = 0;
                         enable2 = 0;
                         write = 0;
                         comp = 0;
                     end
-            `ALLOC_2: begin
-                        next_state = `ALLOC_3;
+					
+            ALLOC_2: begin
+                        next_state = ALLOC_3;
+						
                         // Memory access settings
                         wr = st_reg & (offset == 3'h4);
                         rd = ld_reg | (st_reg & (offset != 3'h4));
                         mem_addr = {address_reg[15:3], 3'h4};
                         mem_data = data_in_sys_reg;
+						
                         // Cache inputs
                         comp = 0;
                         write = 1;
                         tag_in = tag;
-                        cache_data_in = ld_reg | (st_reg & (offset != 3'h0)) ? data_in_mem : data_in_sys_reg;
+                        cache_data_in = (ld_reg | (st_reg & (offset != 3'h0))) ? data_in_mem : data_in_sys_reg;
                         valid_in = 1;
                         enable1 = replace_enable_1_reg;
                         enable2 = replace_enable_2_reg;
                         offset_in = 3'h0;
+						
                         // mem_system outputs
                         sys_stall = 1;
                         data_out_sys = (offset == 3'h0) ? data_in_mem : data_out_sys;
-                        done = (offset == 3'h0) ? 1'h1 : 1'h0;
+                        done = (offset == 3'h0);
                     end
-            `ALLOC_3: begin
-                        next_state = `AW_2;
+					
+            ALLOC_3: begin
+                        next_state = AW_2;
+						
                         // Memory access settings
                         wr = st_reg & (offset == 3'h6);
                         rd = ld_reg | (st_reg & (offset != 3'h6));
                         mem_addr = {address_reg[15:3], 3'h6};
                         mem_data = data_in_sys_reg;
+						
                         // Cache Inputs
                         comp = 0;
                         write = 1;
                         tag_in = tag;
-                        cache_data_in = ld_reg | (st_reg & (offset != 3'h2)) ? data_in_mem : data_in_sys_reg;
+                        cache_data_in = (ld_reg | (st_reg & (offset != 3'h2))) ? data_in_mem : data_in_sys_reg;
                         valid_in = 1;
                         enable1 = replace_enable_1_reg;
                         enable2 = replace_enable_2_reg;
                         offset_in = 3'h2;
+						
                         // mem_system outputs
                         sys_stall = 1;
                         data_out_sys = (offset == 3'h2) ? data_in_mem : data_out_sys;
-                        done = (offset == 3'h2) ? 1'h1 : 1'h0;
+                        done = (offset == 3'h2);
                     end
-            `AW_2: begin // Done with memory accesses - waiting for outputs
-                        next_state = `AW_3;
+					
+            AW_2: begin
+                        next_state = AW_3;
                         wr = 0;
                         rd = 0;
+						
                         // Cache Inputs
                         comp = 0;
                         write = 1;
                         tag_in = tag;
-                        cache_data_in = ld_reg | (st_reg & (offset != 3'h4)) ? data_in_mem : data_in_sys_reg;
+                        cache_data_in = (ld_reg | (st_reg & (offset != 3'h4))) ? data_in_mem : data_in_sys_reg;
                         valid_in = 1;
                         enable1 = replace_enable_1_reg;
                         enable2 = replace_enable_2_reg;
                         offset_in = 3'h4;
+						
                         // Mem system outputs
                         sys_stall = 1;
                         data_out_sys = (offset == 3'h4) ? data_in_mem : data_out_sys;
-                        done = (offset == 3'h4) ? 1'h1 : 1'h0;
+                        done = (offset == 3'h4);
                     end
-            default: begin //AW_3
-                        next_state = `IDLE;
+            default: begin
+                        next_state = IDLE;
                         wr = 0;
                         rd = 0;
                         comp = 0;
                         write = 1;
                         tag_in = tag;
-                        cache_data_in = ld_reg | (st_reg & (offset != 3'h6)) ? data_in_mem : data_in_sys_reg;
+                        cache_data_in = (ld_reg | (st_reg & (offset != 3'h6))) ? data_in_mem : data_in_sys_reg;
                         valid_in = 1;
                         enable1 = st_reg ? replace_enable_1_reg : replace_enable_1_reg;
                         enable2 = st_reg ? replace_enable_2_reg : replace_enable_2_reg;
                         offset_in = 3'h6;
+						
                         // Mem system outputs
                         sys_stall = 1;
                         data_out_sys = (offset == 3'h6) ? data_in_mem : data_out_sys;
-                        done = (offset == 3'h6) ? 1'h1 : 1'h0;
+                        done = (offset == 3'h6);
                     end
         endcase
     end
