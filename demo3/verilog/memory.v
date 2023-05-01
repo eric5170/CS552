799f84009b5068b0f8a32c35859d94bc76ad6fad
jathao <91100446+jathao@users.noreply.github.com>
2023-05-01 15:33:31 -0500

    Brought Memory up to date

--- demo3/verilog/memory.v
+++ demo3/verilog/memory.v
@@ -1,14 +1,23 @@
 /*
-   CS/ECE 552 Spring '22
+   CS/ECE 552 Spring '23
   
    Filename        : memory.v
    Description     : This module contains all components in the Memory stage of the 
                      processor.
 */
-`default_nettype none
-module memory (/* TODO: Add appropriate inputs/outputs for your memory stage here*/);
 
-   // TODO: Your code here
+module memory (clk, rst, isHalt, isMemWrite, ALU_res, writeData, isMemRead, rd_data);
+
+	input wire clk, rst, isHalt, isMemWrite, isMemRead;
+	input wire [15:0] writeData, ALU_res;				
+	output wire[15:0] rd_data;	
+
+	wire memRoW;
+	assign memRoW = isMemRead | isMemWrite;
+
+
+	memory2c DATA_MEM(.data_out(rd_data), .data_in(writeData), .addr(ALU_res), .enable(memRoW),
+					.wr(isMemWrite), .createdump(isHalt), .clk(clk), .rst(rst));
+
    
 endmodule
-`default_nettype wire
